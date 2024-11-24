ba70fabcc40349e8a0984701019b18f4e4915c6e
woodchenwu <woodchenwu@tencent.com>
2024-11-24 22:38:00 +0800

    RM NET CONFIG

--- examples/train_demo.sh
+++ examples/train_demo.sh
@@ -1,41 +1,5 @@
 #!/bin/bash
 
-NET_TYPE="high"
-RESTART_CONFIG="false"
-CHECK_INTERVAL=1200
-export NCCL_IB_TIMEOUT=24
-export NCCL_PROFILE_PRIMS=0
-export NCCL_PROFILE_PRIMS_ENABLE=0
-export NCCL_NVLS_ENABLE=0
-if [[ "${NET_TYPE}" = "low" ]]; then
-    export NCCL_SOCKET_IFNAME=eth1
-    export NCCL_IB_GID_INDEX=3
-    export NCCL_IB_HCA=mlx5_2:1,mlx5_2:1
-    export NCCL_IB_SL=3
-    export NCCL_CHECK_DISABLE=1
-    export NCCL_P2P_DISABLE=0
-    export NCCL_LL_THRESHOLD=16384
-    export NCCL_IB_CUDA_SUPPORT=1
-else
-    export NCCL_IB_GID_INDEX=3
-    export NCCL_IB_SL=3
-    export NCCL_CHECK_DISABLE=1
-    export NCCL_P2P_DISABLE=0
-    export NCCL_IB_DISABLE=0
-    export NCCL_LL_THRESHOLD=16384
-    export NCCL_IB_CUDA_SUPPORT=1
-    export NCCL_SOCKET_IFNAME=bond1
-    export UCX_NET_DEVICES=bond1
-    export NCCL_TOPO_AFFINITY=0
-    export NCCL_IB_HCA=mlx5_bond_1,mlx5_bond_5,mlx5_bond_3,mlx5_bond_7,mlx5_bond_4,mlx5_bond_8,mlx5_bond_2,mlx5_bond_6
-    export NCCL_COLLNET_ENABLE=0
-    export SHARP_COLL_ENABLE_SAT=0
-    export NCCL_NET_GDR_LEVEL=2
-    export NCCL_IB_QPS_PER_CONNECTION=4
-    export NCCL_IB_TC=160
-    export NCCL_PXN_DISABLE=0
-fi
-
 export HOST_GPU_NUM=8
 # 当前机器ip
 # export LOCAL_IP=$LOCAL_IP
